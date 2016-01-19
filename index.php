44ee4199135548c162d4296c340e681eda5aa186
Kassandre PEDRO <kpedro@e3r7p18.42.fr>
2016-01-19 17:01:57 +0100

    basic front

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+	<meta charset="utf-8" />
+		<title>CAMAGRU</title>
+		<link rel="stylesheet" type="text/css" href="camagru.css" media="all"/>
+		<link href='https://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
+	</head>
+
+	<body>
+		<header>
+			<div class="dat_header">
+				<h1>CAMAGRU</h1>
+			</div>
+		</header>
+
+	</body>
+
+	<footer>
+		<div class="dat_footer">
+			
+		</div>
+	</footer>
+</html>
