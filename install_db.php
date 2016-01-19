b969fc495ffd4989945247fb391c0b122fa7514b
Jeremy LEFEBVRE--AGNES <jeremy.la@me.com>
2016-01-20 00:31:27 +0100

    init db for correction done

--- install_db.php
+++ install_db.php
@@ -11,11 +11,11 @@ else
 	try
 	{
 		$pdo = new PDO('mysql:host=127.0.0.1', $user, $password);
-		$DATABASE = $pdo->exec("CREATE DATABASE camagru");
+		$DATABASE = $pdo->exec("CREATE DATABASE IF NOT EXISTS camagru");
 
 		if ($DATABASE)
 		{
-			echo "Database : '$DATABASE' created";
+			echo "Database : CAMAGRU created".PHP_EOL;
 		}
 		else
 		{
@@ -30,18 +30,13 @@ else
 	try
 	{
 		$pdo = new PDO('mysql:host=127.0.0.1;dbname=camagru', $user, $password);
-		$TABLE = $db->exec("CREATE TABLE IF NOT EXISTS users(`id` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL, `login` varchar(255) UNIQUE NOT NULL, `email` varchar(255) NOT NULL UNIQUE, `mdp` varchar(255) NOT NULL");
-		if ($TABLE)
-		{
-		}
-		else
-		{
-			die(print_r($pdo->errorInfo(), true));
-		}
 	}
 	catch(PDOException $e)
 	{
 		die("DB ERROR: ". $e->getMessage());
 	}
-
+	$TABLE = $pdo->exec("CREATE TABLE IF NOT EXISTS users(id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL, login varchar(255) UNIQUE NOT NULL, email varchar(255) NOT NULL UNIQUE, mdp varchar(255) NOT NULL);");
+	echo "Tables: USERS created".PHP_EOL;
+	$pdo->query("INSERT INTO users (login, email, mdp) VALUES('JeremDev', 'jelefebv@student.42.fr', '36a4e012bd9583858a5a6fa15f58bd86a25af266d3a4344f1ec2018b778f29ba83be86eb45e6dc204e11276f4a99eff4e2144fbe15e756c2c88e999649aae7d94')");
+}
 ?>
