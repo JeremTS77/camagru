7417012e8977b32dd98eed376868ff6ff8dac02b
Kassandre PEDRO <kpedro@e3r8p16.42.fr>
2016-01-19 23:28:04 +0100

    edit

---
+++ entete.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+	<meta charset="utf-8" />
+		<title>CAMAGRU</title>
+		<link rel="stylesheet" type="text/css" href="camagru.css" media="all"/>
+		<link href='https://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
+	</head>
+
+	<body>
+		<header>
+			<div class="dat_header">
+				<h1>CAMAGRU</h1>
+			</div>
+		</header>
+
+		<div class= "form">
+			<section class="loginform cf">
+			<form name="login" action="index_submit" method="post" accept-charset="utf-8">
+        		<div class="email">
+        			<label for="usermail">Login</label>
+        			<input name="login" placeholder="Login" required>
+        		</div>
+
+        		<div class="password">
+        			<label for="password">Password</label>
+        			<input type="password" name="password" placeholder="password" required>
+        		</div>
+
+        		<input type="submit" value="Login">
+			</form>
+			</section>
+		</div>
+
+	</body>
+
+	<footer>
+		<div class="dat_footer">
+			
+		</div>
+	</footer>
+</html>
