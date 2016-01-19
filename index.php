8ec77d45526ef8939fc3e157d5147dcad2f61ba5
Kassandre PEDRO <kpedro@e3r7p18.42.fr>
2016-01-19 18:34:20 +0100

    connexion form

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<html>
-	<head>
-	<meta charset="utf-8" />
-		<title>CAMAGRU</title>
-		<link rel="stylesheet" type="text/css" href="camagru.css" media="all"/>
-		<link href='https://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
-	</head>
+<?php
 
-	<body>
-		<header>
-			<div class="dat_header">
-				<h1>CAMAGRU</h1>
-			</div>
-		</header>
+include 'entete.php';
 
-	</body>
-
-	<footer>
-		<div class="dat_footer">
-			
-		</div>
-	</footer>
-</html>
+?>
