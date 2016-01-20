48aa2cd57ab98ae2dbceb3f34833f461b92af9f2
Jeremy LEFEBVRE--AGNES <jeremy.la@me.com>
2016-01-20 15:47:02 +0100

    install_db totaly done

--- install_db.php
+++ install_db.php
@@ -42,6 +42,5 @@ else
 	$pdo->query("INSERT INTO users (login, email, mdp) VALUES('JeremDev', 'jelefebv@student.42.fr', '6a4e012bd9583858a5a6fa15f58bd86a25af266d3a4344f1ec2018b778f29ba83be86eb45e6dc204e11276f4a99eff4e2144fbe15e756c2c88e999649aae7d94')");
 	$pdo->query("INSERT INTO users (login, email, mdp) VALUES('KassDev', 'kpedro@student.42.fr', 'a8984fe4de44af6af44ee9e1ba250a00a4ccc20d763ff4738df389a9a240c8c42a12c9f6324161d7b53cb3eae9db768414dda6ea07ad2a4a207a6bcba29fb9bf')");
 	$pdo = null;
-	`php -S localhost:8000 client`;
 }
 ?>
